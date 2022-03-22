fdf99b22b3e4411e75610ee20145c701f59320d7
erik schievenin <erik.schievenin@gmail.com>
2022-03-22 18:22:02 +0100

    Exercise complete

--- BONUS 2/script.js
+++ BONUS 2/script.js
@@ -1,5 +1,33 @@
 console.log('JS OK!');
 
 // EXERCISE
-// Scrivi una funzione che fonda due array (aventi lo stesso numero di elementi) prendendo alternativamente gli elementi
-// da uno e dall’altro, es. [a,b,c], [1,2,3] → [a,1,b,2,c,3].
+// Scrivi una funzione che accetti tre argomenti: un array e due numeri (“a” più piccolo di “b” e “b” grande al
+// massimo quanto il numero di elementi dell’array). La funzione ritornerà un nuovo array con i valori che hanno
+// la posizione compresa tra “a” e “b”
+
+// Compresa tra “a” e “b” intende che se a=2 e b=5 prende le posizioni 2,3,4,5 o soltanto 3 e 4?
+
+const arrayStart = ['z', 'B', 'O', 'O', 'L', 'E', 'A', 'N', 'z']
+
+const firstElement = 1;
+const secondElement = 9;
+
+const finalResult = exercise(arrayStart, firstElement, secondElement);
+
+console.log(finalResult);
+
+
+
+// funzione richiesta
+
+function exercise(array, a, b) {
+
+    const arrayFinal = [];
+
+    for (let i = a; i < b - 1; i++) {
+
+        arrayFinal.push(array[i])
+    }
+
+    return arrayFinal;
+}
