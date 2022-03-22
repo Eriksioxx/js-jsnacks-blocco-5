b3e18c6f991cf22c440a5f98d2cd5c4fb1f227e3
erik schievenin <erik.schievenin@gmail.com>
2022-03-22 16:46:16 +0100

    exercise 2 complete

--- Js-snack 2/script.js
+++ Js-snack 2/script.js
@@ -1,15 +1,5 @@
 console.log('JS OK!');
 
-// Scrivi una funzione che accetti una stringa come argomento e la ritorni girata (es. Ciao -> oaiC)
-
-// BONUS
-// Scrivi una funzione che fonda due array (aventi lo stesso numero di elementi) prendendo alternativamente gli elementi da uno e
-// dall’altro, es. [a,b,c], [1,2,3] → [a,1,b,2,c,3].
-
-// Scrivi una funzione che accetti tre argomenti: un array e due numeri (“a” più piccolo di “b” e “b” grande al massimo quanto
-// il numero di elementi dell’array). La funzione ritornerà un nuovo array con i valori che hanno la posizione compresa tra “a” e “b”
-
-
 // EXERCISE
 
 // Crea 10 oggetti che rappresentano una zucchina. Dividi in due array separati le zucchine che misurano meno o più di 15cm.
